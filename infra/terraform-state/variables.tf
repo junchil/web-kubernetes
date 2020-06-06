62da997fa01ff9b322ec505644a9553c62bcdaae
junchil <stevejc.liu@gmail.com>
2020-06-06 17:18:35 +1000

    Update variables.tf

--- infra/terraform-state/variables.tf
+++ infra/terraform-state/variables.tf
@@ -3,14 +3,6 @@ variable "region" {
   default     = "ap-southeast-1"
 }
 
-variable "access_key" {
-  default = "AKIAZ2TPUXKA7TG6HO7T"
-}
-
-variable "secret_key" {
-  default = "vI74FWbYfpAW4CtrvemN10lqeDzO29ZKzM67JUms"
-}
-
 # This should be the same variable you define in the root variables.tf
 variable "cluster-name" {
   description = "Cluster Name"
