a11d06ad9e4f47ea0f39191b3b80fc078c82da5c
Steve Liu <ljc1993917@gmail.com>
2020-06-06 20:13:08 +1000

    WEBKUBE-0001 Add terraform infa (#1)

---
+++ infra/eks/output.tf
@@ -0,0 +1,15 @@
+output "kubeconfig" {
+  value = module.eks.kubeconfig
+}
+
+output "config_map_aws_auth" {
+  value = module.eks.config_map_aws_auth
+}
+
+output "Kubenetes-server-ip" {
+  value = module.bastion-host.elastic_ip
+}
+
+output "cluster-name" {
+  value = module.eks.cluster-name
+}
