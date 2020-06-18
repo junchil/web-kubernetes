34b69b2d69571db9d036f3f257359e9d7d47b309
Steve Liu <ljc1993917@gmail.com>
2020-06-18 20:36:31 +1000

    WEBKUBE-0007 Upgrade cluster version to 1.16 (#6)

--- infra/eks/cluster/locals.tf
+++ infra/eks/cluster/locals.tf
@@ -7,9 +7,9 @@ locals {
   common_tags = {
     "cluster_name"  = var.cluster-name
     "comment"       = "Managed by Terraform"
-    "tf_module_ref" = lookup(data.external.git_release.result, "ref", "unknown")
-    "tf_module_url" = lookup(data.external.git_release.result, "url", "unknown")
-    "tf_module_sha" = lookup(data.external.git_release.result, "sha", "unknown")
+    # "tf_module_ref" = lookup(data.external.git_release.result, "ref", "unknown")
+    # "tf_module_url" = lookup(data.external.git_release.result, "url", "unknown")
+    # "tf_module_sha" = lookup(data.external.git_release.result, "sha", "unknown")
   }
 }
 
